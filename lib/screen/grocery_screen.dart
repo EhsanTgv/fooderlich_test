450a82af7cdc78bf580d119f4c695b04585443d9
EhsanTgv <EhsanTgv@gmail.com>
2022-07-21 17:04:20 +0300

    Managing the grocery items

--- lib/screen/grocery_screen.dart
+++ lib/screen/grocery_screen.dart
@@ -1,12 +1,31 @@
 import 'package:flutter/material.dart';
-
 import 'empty_grocery_screen.dart';
+import 'package:provider/provider.dart';
+import '../models/models.dart';
 
 class GroceryScreen extends StatelessWidget {
-  const GroceryScreen({ Key? key }) : super(key: key);
+  const GroceryScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const EmptyGroceryScreen();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {},
+      ),
+      body: buildGroceryScreen(),
+    );
+  }
+
+  Widget buildGroceryScreen() {
+    return Consumer<GroceryManager>(
+      builder: ((context, value, child) {
+        if (value.groceryItems.isNotEmpty) {
+          return Container();
+        } else {
+          return const EmptyGroceryScreen();
+        }
+      }),
+    );
   }
 }
